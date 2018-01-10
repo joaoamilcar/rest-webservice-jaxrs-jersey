443c810f935004917bfc7d9a4e78d2575bda1792
João Amílcar <joaoamilcar@gmail.com>
2018-01-10 20:42:11 -0300

    deserializing an object back from xml with xstream

--- src/test/java/br/com/alura/loja/ClienteTest.java
+++ src/test/java/br/com/alura/loja/ClienteTest.java
@@ -7,24 +7,30 @@ import javax.ws.rs.client.WebTarget;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.thoughtworks.xstream.XStream;
+
+import br.com.alura.loja.modelo.Carrinho;
+
 public class ClienteTest {
 	
-	@Test
-	public void testaQueAConexaoComOServidorFunciona() {
-		Client cliente = ClientBuilder.newClient();
-		WebTarget target = cliente.target("http://www.mocky.io"); // URI base de requisicoes
-		String conteudo = target.path("/v2/52aaf5deee7ba8c70329fb7d").request().get(String.class);
-		// System.out.println(conteudo);
-		Assert.assertTrue(conteudo.contains("<rua>Rua Vergueiro 3185, 8 andar</rua>"));
-	}
-	
-	@Test
+	/*@Test
 	public void testaQueAConexaoComOServidorLocalFuncionaNoPathDeProjetos() {
 		Client cliente = ClientBuilder.newClient();
 		WebTarget target = cliente.target("http://localhost:8080"); // URI base de requisicoes
 		String conteudo = target.path("/projetos").request().get(String.class);
 		// System.out.println(conteudo);
 		Assert.assertTrue(conteudo.contains("<nome>Minha loja</nome>"));
+	}*/
+	
+	@Test
+	public void testaQueBuscarUmCarrinhoTrazOCarrinhoEsperado() {
+		Client cliente = ClientBuilder.newClient();
+		WebTarget target = cliente.target("http://localhost:8080"); // URI base de requisicoes
+		String conteudo = target.path("/carrinhos").request().get(String.class);
+		
+		// System.out.println(conteudo);
+		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+		Assert.assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
 	}
 
 }
