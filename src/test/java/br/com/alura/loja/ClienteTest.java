85b05cec108a060c7426cd3978935f2efa96ebe6
João Amílcar <joaoamilcar@gmail.com>
2018-01-10 23:48:41 -0300

    end-to-end test

--- src/test/java/br/com/alura/loja/ClienteTest.java
+++ src/test/java/br/com/alura/loja/ClienteTest.java
@@ -12,7 +12,7 @@ import org.junit.Test;
 
 import com.thoughtworks.xstream.XStream;
 
-import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Projeto;
 
 public class ClienteTest {
 	
@@ -29,24 +29,14 @@ public class ClienteTest {
 		System.out.println("Server stopped");
 	}
 	
-	/*@Test
+	@Test
 	public void testaQueAConexaoComOServidorLocalFuncionaNoPathDeProjetos() {
 		Client cliente = ClientBuilder.newClient();
 		WebTarget target = cliente.target("http://localhost:8080"); // URI base de requisicoes
 		String conteudo = target.path("/projetos").request().get(String.class);
 		// System.out.println(conteudo);
-		Assert.assertTrue(conteudo.contains("<nome>Minha loja</nome>"));
-	}*/
-	
-	@Test
-	public void testaQueBuscarUmCarrinhoTrazOCarrinhoEsperado() {
-		Client cliente = ClientBuilder.newClient();
-		WebTarget target = cliente.target("http://localhost:8080"); // URI base de requisicoes
-		String conteudo = target.path("/carrinhos").request().get(String.class);
-		
-		// System.out.println(conteudo);
-		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
-		Assert.assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
+		Projeto projeto = (Projeto) new XStream().fromXML(conteudo);
+		Assert.assertEquals("Minha loja", projeto.getNome());
 	}
 
 }
