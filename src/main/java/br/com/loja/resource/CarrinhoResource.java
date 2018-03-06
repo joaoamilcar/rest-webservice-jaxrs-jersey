936fa9c3663395b30c5b4de0f726b5d0fb70984a
João Amílcar <joaoamilcar@gmail.com>
2018-03-06 01:53:49 -0300

    resource creation with POST

--- src/main/java/br/com/loja/resource/CarrinhoResource.java
+++ src/main/java/br/com/loja/resource/CarrinhoResource.java
@@ -1,10 +1,13 @@
 package br.com.loja.resource;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import com.thoughtworks.xstream.XStream;
 
 import br.com.loja.dao.CarrinhoDAO;
 import br.com.loja.model.Carrinho;
@@ -26,6 +29,14 @@ public class CarrinhoResource {
 	public String busca(@PathParam("id") long id) {
 		Carrinho carrinho = new CarrinhoDAO().busca(id);
 		return carrinho.toJson();
+	}
+	
+	@POST
+	@Produces(MediaType.APPLICATION_XML)
+	public String adiciona(String conteudo) {
+		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+		new CarrinhoDAO().adiciona(carrinho);
+		return "<status>sucesso</status>";
 	}
 	
 }
--- src/test/java/br/com/loja/ClienteTest.java
+++ src/test/java/br/com/loja/ClienteTest.java
@@ -2,7 +2,10 @@ package br.com.loja;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.junit.After;
@@ -10,33 +13,50 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.thoughtworks.xstream.XStream;
+import com.google.gson.Gson;
 
+import br.com.loja.model.Carrinho;
+import br.com.loja.model.Produto;
 import br.com.loja.model.Projeto;
 
 public class ClienteTest {
-	
-private HttpServer server;
-	
+
+	private HttpServer server;
+
 	@Before // executa antes de cada teste
 	public void iniciaServidor() {
 		server = Servidor.inicializaServidor();
 	}
-	
+
 	@After // executa apos cada teste
 	public void paraServidor() {
 		server.shutdown();
-		System.out.println("Server parou");
+		System.out.println("Servidor parou");
 	}
-	
+
 	@Test
 	public void testaQueAConexaoComOServidorLocalFuncionaNoPathDeProjetos() {
 		Client cliente = ClientBuilder.newClient();
 		WebTarget target = cliente.target("http://localhost:8080"); // URI base de requisicoes
 		String conteudo = target.path("/projetos/1").request().get(String.class);
 		// System.out.println(conteudo);
-		Projeto projeto = (Projeto) new XStream().fromXML(conteudo);
+		// Projeto projeto = (Projeto) new XStream().fromXML(conteudo);
+		Projeto projeto = new Gson().fromJson(conteudo, Projeto.class);
 		Assert.assertEquals("Minha loja", projeto.getNome());
+	}
+
+	@Test
+	public void testaVerboPost() {
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://localhost:8080");
+		Carrinho carrinho = new Carrinho();
+		carrinho.adiciona(new Produto(314L, "Tablet", 999, 1));
+		carrinho.setRua("Rua Vergueiro");
+		carrinho.setCidade("Sao Paulo");
+		String xml = carrinho.toXML();
+		Entity<String> entity = Entity.entity(xml, MediaType.APPLICATION_XML);
+		Response response = target.path("/carrinhos").request().post(entity);
+		Assert.assertEquals("<status>sucesso</status>", response.readEntity(String.class));
 	}
 
 }
